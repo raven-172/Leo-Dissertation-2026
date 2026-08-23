7071972581e8f42f662bc12c502beb7eda1c6abc
raven-172 <leviethuyhoang172@gmail.com>
2026-08-23 10:53:00 +0100

    update CBAM block, update scr/tasks.py, models yolo26s_rcac3k2.yaml filessetting

--- scr/tasks.py
+++ scr/tasks.py
@@ -185,7 +185,7 @@ def parse_model(d, ch, verbose=True):
             c2 = args[0]
             c1 = ch[f]
             args = [*args[1:]]
-        elif m in frozenset({CA, RCAB, RCAC3k, RCAC3k2}):
+        elif m in frozenset({CA, RCAB, RCAC3k, RCAC3k2, CBAM}):
             c1 = ch[f]
             c2 = c1
             args = [c1, *args]
